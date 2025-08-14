d9917c23eef4846e7f05e17bada3b010c7beced6
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-14 22:19:29 +0100

    add logo

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAspenLogo
+  /// assets/images/aspen_logo.svg
+  static const String assetsImagesAspenLogo = "assets/images/aspen_logo.svg";
+
+  /// Assets for assetsImagesSplashBackground
+  /// assets/images/splash_background.png
+  static const String assetsImagesSplashBackground = "assets/images/splash_background.png";
+}
+
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,31 @@
+import 'package:aspen_travel_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Positioned.fill(
+            child: Image.asset(
+              Assets.assetsImagesSplashBackground,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.20,
+          left: 0,
+          right: 0,
+          child: SvgPicture.asset(Assets.assetsImagesAspenLogo),
+        ),
+      ],
+    );
   }
 }
