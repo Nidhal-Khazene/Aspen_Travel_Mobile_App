0737176e07c5e65ba29aeb99c62f1ff474618ad7
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-14 20:49:41 +0100

    add on generate route function

---
+++ lib/core/helper/on_generate_route.dart
@@ -0,0 +1,11 @@
+import 'package:aspen_travel_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case SplashView.routeName:
+      return MaterialPageRoute(builder: (context) => const SplashView());
+    default:
+      return MaterialPageRoute(builder: (context) => const Scaffold());
+  }
+}
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
+  static const String routeName = "splashView";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SplashViewBody());
+    return const Scaffold(body: SplashViewBody());
   }
 }
